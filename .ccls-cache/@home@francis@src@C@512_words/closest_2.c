c7f38d4991b364c689ec8a70598ca41b017d3df5
nowakf <francisnicholson@gmail.com>
2022-10-21 17:57:57 +0200

    prolly works, too scared to see

--- .ccls-cache/@home@francis@src@C@512_words/closest_2.c
+++ .ccls-cache/@home@francis@src@C@512_words/closest_2.c
@@ -6,13 +6,6 @@
 #define PROB_MAX 97
 // 32 * 3 + 1
 
-#define MIN(a, b, c) ({                                   \
-	 __typeof__(a) _a = (a);                          \
-	 __typeof__(b) _b = (b);                          \
-	 __typeof__(c) _c = (c);                          \
-	 _a < _b ? _a < _c ? _a : _c : _b < _c ? _b : _c; \
-	 })                                               \
-
 typedef struct {
 	uint8_t min_start;
 	uint8_t max_start;
@@ -29,8 +22,7 @@ const utf8 valid[][3] = {
 	[L4] = {{0xf0, 0xf0, 0x90, 0xbf}, {0xf1, 0xf3, 0x80, 0xbf}, {0xf4, 0xf4, 0x80, 0x8f}},
 };
 
-#define INVALID_SPEC (utf8){0}
-#define VARIANT_MAX 3
+#define VARIANT_MAX 4
 
 bool utf8_spec_equal(utf8 a, utf8 b) {
 	return 	a.min_start     == b.min_start
@@ -38,6 +30,8 @@ bool utf8_spec_equal(utf8 a, utf8 b) {
 		&& a.min_follow == b.min_follow
 		&& a.max_follow == b.max_follow;
 }
+
+#define invalid_spec(a) (utf8_spec_equal((a), (utf8){0}))
 
 uint8_t clamp(uint8_t val, uint8_t min, uint8_t max) {
 	return val | min & max;
@@ -51,16 +45,12 @@ void transform(const uint8_t * from, uint8_t *to, int len, int variant) {
 	}
 }
 
-int min_variant(const uint8_t * str, int vlen, uint_t * score) {
-}
-
-
 uint8_t char_distance(uint8_t ch, uint8_t min, uint8_t max) {
 	return __builtin_popcount(ch ^ clamp(ch, min, max));
 }
 
 
-uint8_t codepoint_distance(uint8_t *str, int len, int variant) {
+uint8_t codepoint_distance(const uint8_t *str, int len, int variant) {
 	const utf8 spec = valid[len][variant];
 	uint8_t distance = char_distance(str[0], spec.min_start, spec.max_start);
 	for (int i=1; i<=len; ++i) {
@@ -68,27 +58,26 @@ uint8_t codepoint_distance(uint8_t *str, int len, int variant) {
 	}
 	return distance * 3 / len;
 }
-int characterize(uint8_t *str, int max_len) {
-	int best_score = 0;
-	int best_scoring = 0;
-	for (int len=1; len<=max_len; ++len) {
-		for (int variant=0; variant<VARIANT_MAX; ++variant) {
-			if (utf8_spec_equal(valid[len][variant], INVALID_SPEC)) break;
-			uint8_t score = codepoint_distance(str, len, variant);
-			if (score > best_score) {
-				best_score = score;
-				best_scoring = score;
-			}
+
+int min_variant(const uint8_t * str, int vlen, uint8_t * score) {
+	int variant = 0;
+	for (int i=0; i<VARIANT_MAX; ++i) {
+		utf8 spec = valid[vlen][i];
+		uint8_t new_score = codepoint_distance(str, vlen, i);
+		if (*score < new_score) {
+			*score = new_score;
+			variant = i;
 		}
 	}
-	return best_scoring;
+	return variant;
 }
 
 typedef struct {
-	uint8_t * odds;
-	int     * types;
-	int     * indexes;
-} stats;
+	uint8_t likelyhood;
+	int8_t variant;
+	int index;
+} occupant;
+
 
 bool space_available(uint8_t * buf, int start, int len) {
 	for (int i=start; i<(start+len); ++i) {
@@ -99,61 +88,56 @@ bool space_available(uint8_t * buf, int start, int len) {
 	return true;
 }
 
-stats new_stats(uint8_t * buf, int len) {
-	stats s = {
-		calloc(len * 4, 1),
-		calloc(len * 4, sizeof(int)),
-		calloc(len * 4, sizeof(int)),
-	};
-	if (!s.odds || !s.types || s.indexes) {
-		return (stats){0};
-	}
-	for (int i=0; i<(len*4); ++i) {
-		s.indexes[i] = i;
-	}
-
+void get_occupant_probability(occupant *occupants, const uint8_t *buf, int len){
+	uint8_t p1;
+	int v;
 	for (int i=0; i<len; ++i) {
-		s.odds[i] = PROB_MAX - codepoint_distance(buf+i, L1, 0);
+		occupants[i] = (occupant){
+			.likelyhood = PROB_MAX - codepoint_distance(buf+i, L1, 0),
+			.variant = 0,
+			.index = i,
+		};
 		if (i > (len-1)) continue;
-		s.odds[len+i] = PROB_MAX - codepoint_distance(buf+i, L2, 0);
+		occupants[i+len] = (occupant){
+			.likelyhood = PROB_MAX - codepoint_distance(buf+i, L2, 0),
+			.variant = 0,
+			.index = i+len,
+		};
 		if (i > (len-2)) continue;
-		s.odds[len*2+i] = PROB_MAX - codepoint_distance(buf+i, L3, 0);
+		v = min_variant(buf+i, 3, &p1);
+		occupants[i+len*2] = (occupant){
+			.likelyhood = PROB_MAX - p1,
+			.variant = v,
+			.index = i+len*2,
+		};
 		if (i > (len-3)) continue;
-		s.odds[len*3+i] = PROB_MAX - codepoint_distance(buf+i, L4, 0);
+		v = min_variant(buf+i, 4, &p1);
+		occupants[i+len*3] = (occupant){
+			.likelyhood = PROB_MAX - p1,
+			.variant = v,
+			.index = i+len*3,
+		};
 	}
+}
+int cmp (const void * a, const void * b) {
+	int _a = ((occupant *)a)->likelyhood;
+	int _b = ((occupant *)b)->likelyhood;
+	return _a - _b;
+};
 
-
-	int cmp (const void * a, const void * b) {
-		int pa = index_to_pvalue(&p, *(int *)a, len);
-		int pb = index_to_pvalue(&p, *(int *)b, len);
-		return pa - pb;
-	};
-
-	qsort(s.indexes, len*4, sizeof(int), &cmp);
-
-	//fill out array with 0xff (invalid utf8);
-	//go over indexes inserting in order if there is enough space
-	
-	uint8_t * out = malloc(len);
+void closest_transformation(occupant *probable_occupants, const uint8_t *in, uint8_t *out, int len) {
+	qsort(probable_occupants, len*4, sizeof(int), &cmp);
 	memset(out, 0xff, len);
 	int filled = 0;
 	int i = 0;
 	while (filled < (len * 4) && i < (len * 4)) {
-		int index = s.indexes[i];
-		int run_len = index / len + 1;
-		int start = index % len;
-		if (space_available(out, start, run_len)) {
-			transform(buf, out, run_len, s.types[i]);
+		occupant o = probable_occupants[i++];
+		int run_len = o.index / len + 1;
+		int buf_index = o.index % len;
+		if (space_available(out, buf_index, run_len)) {
+			transform(in+buf_index, out+buf_index, run_len, o.variant);
 			filled += run_len;
 		}
-		i++;
 	}
-	return s;
-}
-
-void free_stats(stats *s) {
-	free(s->odds);
-	free(s->types);
-	free(s->indexes);
 }
 
